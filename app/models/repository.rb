339842a62f18a0ca4f0f50f0212e9deddf9359cf
Jonathan Stone <stone.j@gordon0darby.com>
2026-02-19 12:44:19 -0600

    Fixed Repository class.

--- app/models/repository.rb
+++ app/models/repository.rb
@@ -1,23 +1,30 @@
-class Actor < ApplicationRecord
+class Repository < ApplicationRecord
   has_many :push_events, dependent: :nullify
 
   validates :github_id, presence: true, uniqueness: true
-  validates :login, presence: true
+  validates :full_name, presence: true
 
   scope :needs_refresh, -> { where("last_enriched_at IS NULL OR last_enriched_at < ?", 24.hours.ago) }
   scope :already_enriched, -> { where("last_enriched_at > ?", 24.hours.ago) }
+  scope :by_language, ->(lang) { where(language: lang) }
+  scope :popular, -> { order(stars_count: :desc) }
 
-  def self.find_or_create_from_payload(actor_data)
-    return nil if actor_data.blank?
+  def self.find_or_create_from_payload(repo_data)
+    return nil if repo_data.blank?
 
-    github_id = actor_data['id']
-    login = actor_data['login']
+    github_id = repo_data['id']
+    full_name = repo_data['full_name']
 
-    find_or_create_by(github_id: github_id) do |actor|
-      actor.login = login
-      actor.avatar_url = actor_data['avatar_url']
-      actor.profile_url = actor_data['html_url']
-      actor.actor_type = actor_data['type']
+    find_or_create_by(github_id: github_id) do |repo|
+      repo.full_name = full_name
+      repo.name = repo_data['name']
+      repo.description = repo_data['description']
+      repo.language = repo_data['language']
+      repo.repository_url = repo_data['html_url']
+      repo.clone_url = repo_data['clone_url']
+      repo.default_branch = repo_data['default_branch']
+      repo.stars_count = repo_data['stargazers_count']
+      repo.forks_count = repo_data['forks_count']
     end
   end
 
